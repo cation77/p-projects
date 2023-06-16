7c0fbb3b00f0351fecbba6461cf23d183f9ae087
guojian <2502585266@qq.com>
2023-06-16 17:16:43 +0800

    chore: test react debounce

--- packages/react/src/App.tsx
+++ packages/react/src/App.tsx
@@ -1,11 +1,22 @@
-import { sayHi } from "@pnpm-monorepo/shared";
-import Upload from '@/components/upload';
+import React, { useEffect } from 'react'
+import { isPlainObject, debounce } from '@pnpm-monorepo/shared'
+import Upload from '@/components/upload'
 import './App.css'
 
 function App() {
-  sayHi("Vedansh");
+  useEffect(() => {
+    console.log('{} isPlainObject->', isPlainObject({}))
+    console.log('react isPlainObject->', isPlainObject(Object.create(React)))
+  })
+
+  const handleClick = debounce<[React.MouseEvent, string]>((e, name) => {
+    console.log('handleClick', e, name)
+  }, 2000)
   return (
-    <Upload />
+    <>
+      <h3 onClick={(e) => handleClick(e, 'name')}>点击</h3>
+      <Upload />
+    </>
   )
 }
 
