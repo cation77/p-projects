abf15cf86440d4a3c7eba76ea46ac84e99ea27d2
guojian <2502585266@qq.com>
2023-06-27 15:22:25 +0800

    express: 文件切片上传及合并

--- projects/express/routes/upload.js
+++ projects/express/routes/upload.js
@@ -5,37 +5,63 @@ const path = require('path')
 // const { Buffer } = require('buffer')
 
 const router = express.Router()
-const staticPath = path.join(process.cwd(), '/public/flies')
+const staticPath = path.join(process.cwd(), '/public/files')
 const staticTemporary = path.join(process.cwd(), '/public/temporary')
 
 /* GET users listing. */
-router.get('/:id', function (req, res) {
-  console.log(req.params.id)
-  res.send(`${req.params.id} respond with a resource`)
+router.get('/detail', function (req, res) {
+  console.log(req.query.id, fs.existsSync(`${staticPath}/文档`))
+  res.send(`${req.query.id} respond with a resource`)
 })
 
 router.post('/', function (req, res) {
   const form = new multiparty.Form()
-  form.encoding = 'utf-8'
   form.parse(req, function (err, fields, files) {
     if (err) {
-      console.log(err)
       res.send({
         code: 500,
-        message: `${err} 切片上传失败`
+        message: `${err.message},切片上传失败`
       })
       return
     }
-    console.log(fields, files)
-    // const fileName = fields.fileName[0]
-    // const hash = fields.hash
-    // const chunk = fields.chunk
-    // const dir = `${staticTemporary}/${fileName}`
-    // try {
-    //   if (!fs.existsSync(dir)) fs.mkdirSync(dir)
-    // } catch (error) {
-    //   res.status(500).send(`${fileName} 切片上传失败`)
-    // }
+    const filename = fields.name[0]
+    const hash = fields.hash[0]
+    const chunk = files.chunk[0]
+    const dir = `${staticTemporary}/${hash}`
+    try {
+      if (!fs.existsSync(dir)) fs.mkdirSync(dir)
+      const buffer = fs.readFileSync(chunk.path)
+      const stream = fs.createWriteStream(`${dir}/${filename}`)
+      stream.write(buffer)
+      stream.close()
+      res.send({
+        code: 200,
+        message: `${filename}切片上传成功`
+      })
+    } catch (error) {
+      res.send({ code: 500, message: `${filename}切片上传失败` })
+    }
   })
+})
+
+router.get('/merge', function (req, res) {
+  try {
+    const { hash, filename } = req.query
+    let len = 0
+    const bufferList = fs
+      .readdirSync(`${staticTemporary}/${hash}`)
+      .map((chunk) => {
+        const buffer = fs.readFileSync(`${staticTemporary}/${hash}/${chunk}`)
+        len += buffer.length
+        return buffer
+      })
+    const fileBuffer = Buffer.concat(bufferList, len)
+    const stream = fs.createWriteStream(`${staticPath}/${filename}`)
+    stream.write(fileBuffer)
+    stream.close()
+    res.send({ code: 200, message: `${filename}合并成功` })
+  } catch (error) {
+    res.send({ code: 500, message: `${filename}合并文件失败` })
+  }
 })
 module.exports = router
